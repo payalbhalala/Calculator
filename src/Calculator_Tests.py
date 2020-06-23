27c7758b3551feee50682c93de03249eedacbceb
Payal Bhalala <pkb7@njit.edu>
2020-06-23 19:05:30 -0400

    Feature: Added multiplication csv file and updated multiplication method

--- src/Calculator_Tests.py
+++ src/Calculator_Tests.py
@@ -33,8 +33,10 @@ class MyTestCase(unittest.TestCase):
             self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_divide_method_calculator(self):
-        self.assertEqual(self.calculator.divide(4, 2), 2)
-        self.assertEqual(self.calculator.result, 2)
+        test_data_divide = CsvReader('/src/Division.csv').data
+        for row in test_data_divide:
+            self.assertEqual(self.calculator.divide(int(row['Value 1']), int(row['Value 2'])), float(row['Result']))
+            self.assertEqual(self.calculator.result, float(row['Result']))
 
     def test_square_method_calculator(self):
         self.assertEqual(self.calculator.square(2), 4)
